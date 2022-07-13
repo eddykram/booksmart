b077f7b33acd8434c5749495719258beab2c723a
Edward <eddy.kram@gmail.com>
2022-07-13 14:13:51 -0400

    footer and image changes

--- remove-text.js
+++
@@ -1,8 +0,0 @@
-function eraseText() {
-    delay(1000).then(() => document.getElementById('output').value = "");
-}
-
-// waits a certain amount of time before doing something (1 second = 1000)
-function delay(time) {
-    return new Promise(resolve => setTimeout(resolve, time));
-  }
